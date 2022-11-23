f51c6c2efe71149ea10a1c7aee4922bf0368bd7e
Daniel <danya.ostap@gmail.com>
2022-11-24 01:33:03 +0200

    Added useEffect with routes get request to the routes page

--- src/components/AllRoutes.js
+++ src/components/AllRoutes.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const AllRoutes = () => {
+    const [routes, setRoutes] = useState(null);
+
+    useEffect(() => {
+        axios({
+            url: 'https://localhost:8443/api/v1/routes',
+            method: 'get',
+            headers: {
+                'Authorization': "Bearer_" + Cookies.get("_auth")
+            }
+         })
+         .then(response => {
+            console.log(response.data)
+         }) 
+         .catch(err => {
+            console.log(err);
+         });
+    }, [])
+
     return ( 
         <div>Routes</div>
     );
