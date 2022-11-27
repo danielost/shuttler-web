6aeb95233bd9748566fbc285bbfc1d1d2a726da7
Daniel <danya.ostap@gmail.com>
2022-11-27 13:33:03 +0200

    Added edit routes handle

--- src/components/OrganizerPanelRoutes.js
+++ src/components/OrganizerPanelRoutes.js
@@ -7,19 +7,27 @@ import EditableRow from "./tableRows/EditableRow";
 import ReadOnlyRow from "./tableRows/ReadOnlyRow";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { BiAddToQueue } from "react-icons/bi";
 
 const OrganizerPanelRoutes = () => {
   const [routes, setRoutes] = useState(null);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const allowedTypes = ["bus", "trolleybus", "tram"];
+
   const [addFormData, setAddFormData] = useState({
     number: "",
     type: "",
   });
   const [editFormData, setEditFormData] = useState({
+    id: "",
     number: "",
     type: "",
   });
 
+  const [message, setMessage] = useState("");
   const [currNumber, setCurrNumber] = useState("");
   const [editRouteId, setEditRouteId] = useState(null);
 
@@ -47,6 +55,28 @@ const OrganizerPanelRoutes = () => {
       number: editFormData.number,
     };
 
+    if (allowedTypes.indexOf(editedRoute.type) === -1) {
+      setMessage(
+        "Error: " +
+          editedRoute.type +
+          " -bad type: allowed types are {" +
+          allowedTypes +
+          "}"
+      );
+      handleShow();
+      setEditRouteId(null);
+      return 0;
+    }
+
+    let isNumberTaken = routes.find(
+      (route) => route.id !== editRouteId && route.number === editedRoute.number
+    );
+    if (isNumberTaken !== null) {
+      setMessage("Error: number " + editedRoute.number + " is taken");
+      handleShow();
+      setEditRouteId(null);
+      return 0;
+    }
     const newRoutes = [...routes];
 
     const index = routes.findIndex((route) => route.id === editRouteId);
@@ -56,6 +86,30 @@ const OrganizerPanelRoutes = () => {
 
     setRoutes(newRoutes);
     setEditRouteId(null);
+
+    axios({
+      url:
+        "https://localhost:8443/api/v1/organizer/updateRoute/" + editedRoute.id,
+      method: "put",
+      data: {
+        type: editedRoute.type,
+        number: editedRoute.number,
+      },
+      headers: {
+        Authorization: "Bearer_" + Cookies.get("_auth"),
+      },
+    })
+      .then((response) => {
+        setMessage("Route updated");
+        handleShow();
+        console.log(response.data);
+      })
+      .catch((err) => {
+        setMessage(err);
+        handleShow();
+        console.log(err);
+      });
+    // }
   };
 
   const handleEditClick = (event, route) => {
@@ -63,6 +117,7 @@ const OrganizerPanelRoutes = () => {
     setEditRouteId(route.id);
 
     const formValues = {
+      id: route.id,
       type: route.type,
       number: route.number,
     };
@@ -103,6 +158,17 @@ const OrganizerPanelRoutes = () => {
 
   return (
     <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Operation result</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{message}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Got it
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="organizer-header">
         <h3 style={{ color: "white" }}>Your routes</h3>
         <div style={{ display: "flex", flexDirection: "row" }}>
@@ -136,28 +202,30 @@ const OrganizerPanelRoutes = () => {
             </thead>
             <tbody>
               {routes.map((currRoute) => {
-                // if (
-                //   currRoute.number.toString().indexOf(currNumber) !== -1 ||
-                //   currNumber === ""
-                // ) {
-                if (currRoute.id === editRouteId) {
-                  return (
-                    <EditableRow
-                      editFormData={editFormData}
-                      handleEditFormChange={handleEditFormChange}
-                      handleCancelClick={handleCancelClick}
-                    />
-                  );
-                } else {
-                  return (
-                    <ReadOnlyRow
-                      route={currRoute}
-                      handleEditClick={handleEditClick}
-                      handleDeleteClick={handleDeleteClick}
-                    />
-                  );
+                if (
+                  (currRoute.number !== null &&
+                    currRoute.number.toString().indexOf(currNumber) !== -1) ||
+                  currNumber === ""
+                ) {
+                  if (currRoute.id === editRouteId) {
+                    return (
+                      <EditableRow
+                        editFormData={editFormData}
+                        handleEditFormChange={handleEditFormChange}
+                        handleCancelClick={handleCancelClick}
+                      />
+                    );
+                  } else {
+                    return (
+                      <ReadOnlyRow
+                        route={currRoute}
+                        handleEditClick={handleEditClick}
+                        handleDeleteClick={handleDeleteClick}
+                      />
+                    );
+                  }
                 }
-                // }
+                return <></>;
               })}
             </tbody>
           </Table>
